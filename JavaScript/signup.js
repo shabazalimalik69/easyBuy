5111071b12dc7b40458f5ea57fce8d8c61183243
shabaz ali malik <shabazalimalik69@gmail.com>
2022-12-07 16:56:44 +0530

    Navbar Updated

--- JavaScript/signup.js
+++ JavaScript/signup.js
@@ -1,2 +1,17 @@
 import {navbar} from "../NavbarComponent/navbar.js"
 document.getElementById("Navbar").innerHTML = navbar()
+
+
+ const navToggler = document.querySelector(".hamburger");
+ navToggler.addEventListener("click", navToggle);
+
+ function navToggle() {
+   navToggler.classList.toggle("active");
+   const nav = document.querySelector(".navbar");
+   nav.classList.toggle("open");
+   if (nav.classList.contains("open")) {
+     nav.style.maxHeight = nav.scrollHeight + "px";
+   } else {
+     nav.removeAttribute("style");
+   }
+ }
--- NavbarComponent/navbar.js
+++ NavbarComponent/navbar.js
@@ -1,33 +1,22 @@
-const navbar = ()=>{
+const navbar = () => {
     return ` 
-    <div id="navbar">
-    <div id="logo">
-        <a href="./home.html"><img id="img" src="../images/easyBuy.jpeg" alt="logo"></a>
-    </div>
-    <div id="hamburger">
-    <span></span>
-    <span></span>
-    <span></span>
-    </div>
-    <div id="main_section">
-        <div id="middle_section">
-            <a href="./home.html">Home</a>
-            <a href="./product.html">Products</a>
-        </div>
-       
-        <div id="right_section">
-            <a href="./login.html">Login</a>
-            <a href="./signup.html">SignUp</a>
-            <a href="./cart.html">Cart</a>
-        </div>
-    </div>
-    <div id="hamburgerBox" >
-    <div> <a href="./home.html">Home</a></div>
-    <div> <a href="./product.html">Products</a></div>
-    <div> <a href="./signup.html">SignUp</a></div>
-    <div> <a href="./login.html">Login</a></div>
-    </div>
-</div>
-    `
+      <div class="container">
+            <div class="logo">
+               <a href="home.html"><img class="image" class="image" src="../images/easyBuy.jpeg"/></a>
+            </div>
+            <button type="button" class="hamburger">
+               <span></span>
+            </button>
+            <nav class="navbar">
+               <ul>
+                  <li><a href="home.html" id="home">Home</a></li>
+                  <li><a href="product.html">Products</a></li>
+                  <li><a class="signup" href="signup.html">Signup</a></li>
+                  <li><a  class="login" href="login.html">Login</a></li>
+                  <li><a href="cart.html">Cart</a></li>
+               </ul>
+            </nav>
+      </div>
+    `;
 }
 export {navbar}
