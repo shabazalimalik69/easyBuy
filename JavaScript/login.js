c65af53faaa19b8181b0f1c4617d42f6e6dc686d
sombir5 <sombirverma5@gmail.com>
2022-12-08 11:19:05 +0530

    login

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -14,4 +14,35 @@ document.getElementById("Navbar").innerHTML = navbar()
    } else {
      nav.removeAttribute("style");
    }
- }
+}
+ let form = document.getElementById("form");
+form.addEventListener("submit", LoginData);
+ 
+let array = JSON.parse(localStorage.getItem("clientData"));
+let loggedUser = JSON.parse(localStorage.getItem("loggedUser")) || [];
+
+
+function LoginData(e){
+  e.preventDefault();
+  console.log("hi")
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let flag = false;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].Email === email && array[i].Password===password) {
+      flag = true;
+      loggedUser.push(array[i]);
+      localStorage.setItem("loggedUser",JSON.stringify(loggedUser))
+    }
+  }
+  if (flag) {
+    alert("Login successfully");
+  //  window.location.href="../html/home.html"
+    console.log(loggedUser);
+  } else {
+    alert("Enter correct credentials");
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+  }
+};
+
